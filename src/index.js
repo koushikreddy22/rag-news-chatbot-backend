3fe2ec257583cb31bd958a4a68f65d9f75cb1e73
koushik <koushik.m@unifo.in>
2025-09-16 09:46:48 +0530

    Backend Project setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("RAG Chatbot Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
